730a73bdce37bd797ce144247c065bb0aaa89c4a
Harsh Davda <harsh.davda11@outlook.com>
2025-01-06 15:22:13 +0530

    added package utils

---
+++ ras_common/package/utils.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import importlib
+
+def get_cmake_python_pkg_source_dir(package_name:str):
+    try:
+        module_type = importlib.import_module(package_name)
+        pkg_path = (Path(module_type.__file__)/"..").resolve().absolute()
+        return pkg_path
+    except:
+        return None
+    
--- scripts/test.py
+++ scripts/test.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
-from ras_common.config.loaders.ConfigLoaderBase import ConfigLoaderBase
-from dataclasses import dataclass
-# Example usage
-@dataclass
-class MyConfig(ConfigLoaderBase):
-    param1: float
-    param2: str
+from ras_common.package.utils import get_cmake_python_pkg_source_dir
+# from ras_common.config.loaders.ConfigLoaderBase import ConfigLoaderBase
+# from dataclasses import dataclass
+# # Example usage
+# @dataclass
+# class MyConfig(ConfigLoaderBase):
+#     param1: float
+#     param2: str
 
-@dataclass
-class MyConfig2(ConfigLoaderBase):
-    par1: int
-    par2: MyConfig
+# @dataclass
+# class MyConfig2(ConfigLoaderBase):
+#     par1: int
+#     par2: MyConfig
 
-config_data = {
-    'par1': 42,
-    'par2': {
-        'param1': 42,
-        'param2': 'hello'}
-}
+# config_data = {
+#     'par1': 42,
+#     'par2': {
+#         'param1': 42,
+#         'param2': 'hello'}
+# }
 def main():
-    config_instance = MyConfig2.from_dict(config_data)
-    print(config_instance)
-    print(MyConfig2.to_dict())
+    # config_instance = MyConfig2.from_dict(config_data)
+    # print(config_instance)
+    # print(MyConfig2.to_dict())
+    print(get_cmake_python_pkg_source_dir("ras_common"))
+    print(get_cmake_python_pkg_source_dir("ras_bt_framework"))
 
 if __name__ == "__main__":
     main()

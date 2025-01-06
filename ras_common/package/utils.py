047a2e7b9174b69fa4a700bc2b82ecc25daf7345
Harsh Davda <harsh.davda11@outlook.com>
2025-01-06 15:27:39 +0530

    added pkg path fix

--- ras_common/package/utils.py
+++ ras_common/package/utils.py
@@ -4,7 +4,7 @@ import importlib
 def get_cmake_python_pkg_source_dir(package_name:str):
     try:
         module_type = importlib.import_module(package_name)
-        pkg_path = (Path(module_type.__file__)/"..").resolve().absolute()
+        pkg_path = (Path(module_type.__file__)/"../..").resolve().absolute()
         return pkg_path
     except:
         return None

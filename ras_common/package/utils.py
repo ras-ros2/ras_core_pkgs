005bd9c6ae661621cd86dbdce770c47f69b052f9
Sachin Kumar <sachin.kumar@accelerationrobotics.com>
2025-01-12 17:34:19 +0100

    attribute added to raise the exception

--- ras_common/package/utils.py
+++ ras_common/package/utils.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import importlib
 
-def get_cmake_python_pkg_source_dir(package_name:str):
+def get_cmake_python_pkg_source_dir(package_name: str, raise_exception: bool = False) -> Path | None:
     try:
         module_type = importlib.import_module(package_name)
-        pkg_path = (Path(module_type.__file__)/"../..").resolve().absolute()
+        pkg_path = (Path(module_type.__file__).parent.parent).resolve().absolute()
         return pkg_path
-    except:
+    except Exception as e:
+        if raise_exception:
+            raise RuntimeError(f"Failed to find the package source directory for '{package_name}': {e}")
         return None
-    

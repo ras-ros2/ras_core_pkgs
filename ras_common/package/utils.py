9576aeb77731183b0679789f87c43d7aed5f7ffd
Sachin Kumar <sachin.kumar@accelerationrobotics.com>
2025-01-12 17:50:06 +0100

    changed to no_raise for exception

--- ras_common/package/utils.py
+++ ras_common/package/utils.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import importlib
 
-def get_cmake_python_pkg_source_dir(package_name: str, raise_exception: bool = True) -> Path | None:
+def get_cmake_python_pkg_source_dir(package_name: str, no_raise: bool = False) -> Path | None:
     try:
         module_type = importlib.import_module(package_name)
         pkg_path = (Path(module_type.__file__).parent.parent).resolve().absolute()
         return pkg_path
     except Exception as e:
-        if raise_exception:
+        if not no_raise:
             raise RuntimeError(f"Failed to find the package source directory for '{package_name}': {e}")
         return None

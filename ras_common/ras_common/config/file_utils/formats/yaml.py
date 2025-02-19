9136117760031b2d63ac1eb016bda91d3509ea87
Harsh Davda <harsh.davda11@outlook.com>
2025-02-19 10:41:03 +0530

    path type fix

--- ras_common/ras_common/config/file_utils/formats/yaml.py
+++ ras_common/ras_common/config/file_utils/formats/yaml.py
@@ -6,8 +6,12 @@ from pathlib import Path
 class YamlFormat(BaseFormat):
     @staticmethod
     def load( file_path: Path):
+        if isinstance(file_path, str):
+            file_path = Path(file_path)
         return yaml.safe_load(file_path.open('r'))
     
     @staticmethod
     def dump(data:dict, file_path:Path):
+        if isinstance(file_path, str):
+            file_path = Path(file_path)
         yaml.safe_dump(data, file_path.open('w'))

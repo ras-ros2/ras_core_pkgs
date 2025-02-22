75e598ef5672462c61a96b3a6fb1ceb6c9b09116
Sachin Kumar <sachin.kumar@accelerationrobotics.com>
2025-02-22 12:24:44 +0100

    condition added to check the mqtt broker and file server

--- ras_transport/launch/server.launch.py
+++ ras_transport/launch/server.launch.py
@@ -1,24 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ras_common.config.loaders.ras_config import RasObject
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='ras_transport',
-            executable='file_server.py',
-            name='file_server',
-            output='screen'
-        ),
+    RasObject.init()
+    file_server_ip: str = RasObject.ras.transport.file_server.ip
+    mqtt_broker_ip: str = RasObject.ras.transport.mqtt.ip
+
+    nodes = [
         Node(
             package='ras_transport',
             executable='log_receiver.py',
             name='log_receiver',
-            output='screen'
-        ),
-        Node(
-            package='ras_transport',
-            executable='mqtt_broker.py',
-            name='mqtt_broker',
             output='screen'
         ),
         Node(
@@ -33,4 +26,26 @@ def generate_launch_description():
             name='transport_server_service',
             output='screen'
         )
-    ])
+    ]
+    
+    if mqtt_broker_ip in {"localhost", "127.0.0.1", "0.0.0.0"}:
+        nodes.append(
+            Node(
+                package='ras_transport',
+                executable='file_server.py',
+                name='file_server',
+                output='screen'
+            )
+        )
+    
+    if file_server_ip in {"localhost", "127.0.0.1", "0.0.0.0"}:
+        nodes.append(
+             Node(
+                package='ras_transport',
+                executable='mqtt_broker.py',
+                name='mqtt_broker',
+                output='screen'
+            ),
+        )
+    
+    return LaunchDescription(nodes)
